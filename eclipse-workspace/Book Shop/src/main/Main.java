0cfcbbfe69b86ff80b8abe21c64065ab725570e6
malyga <40186295+malyga@users.noreply.github.com>
2019-02-20 19:58:29 +0100

    ejemplo conexión y consulta

--- eclipse-workspace/Book Shop/src/main/Main.java
+++ eclipse-workspace/Book Shop/src/main/Main.java
@@ -1,10 +1,32 @@
 package main;
 
+import javax.xml.namespace.QName;
+import javax.xml.xquery.*;
+
+import net.xqj.basex.BaseXXQDataSource;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
+		try {
+			XQDataSource xqs = new BaseXXQDataSource();
+			xqs.setProperty("serverName", "localhost");
+			xqs.setProperty("port", "1984");
+			XQConnection conn = xqs.getConnection("USERNAME", "PASSWORD");
+			XQPreparedExpression xqpe = conn.prepareExpression("declare variable $x as xs:string external; $x");
+			
+			xqpe.bindString(new QName("x"), "Hello World!", null);
+			
+			XQResultSequence rs = xqpe.executeQuery();
+			
+			while(rs.next()) {
+				System.out.println(rs.getItemAsString(null));
+			}
+			
+			conn.close();
+		} catch (XQException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
