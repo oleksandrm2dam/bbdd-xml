474f72c5f5741fea37c8ba968d9e376fbc176793
DimitriDzamtoski <47858356+DimitriDzamtoski@users.noreply.github.com>
2019-02-26 19:57:47 +0100

    Add files via upload

---
+++ eclipse-workspace/Book Shop/src/entities/User.java
@@ -0,0 +1,46 @@
+package entities;
+
+public class User {
+
+	private String name;
+	private String password;
+	private String profile;
+
+	public User(String name, String password) {
+		super();
+		this.name = name;
+		this.password = password;
+	}
+
+	public User(String name, String password, String profile) {
+		super();
+		this.name = name;
+		this.password = password;
+		this.profile = profile;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+}
