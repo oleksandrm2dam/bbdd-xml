05de7523f76cfe40b0afcffa39751e7d5848e7a7
malyga <40186295+malyga@users.noreply.github.com>
2019-02-27 20:43:55 +0100

    ejemplo de query funcionando

--- eclipse-workspace/Book Shop/src/main/Main.java
+++ eclipse-workspace/Book Shop/src/main/Main.java
@@ -1,10 +1,30 @@
 package main;
 
+import org.basex.core.BaseXException;
+import org.basex.core.Context;
+import org.basex.core.cmd.*;
+import org.basex.query.QueryProcessor;
+import org.basex.query.iter.Iter;
+import org.basex.query.value.item.Item;
+
 public class Main {
+	
+	static final String DB_PATH = "db/test.xml";
+	
+	public static void main(final String... args) throws Exception {
+	    // Database context.
+	    Context context = new Context();
 
-	public static void main(String[] args) {
-		
-		
-	}
-
+	    // Create a database from a local or remote XML document or XML String
+	    System.out.println("\n* Create a database.");
+	    new CreateDB("DBExample", DB_PATH).execute(context);
+	    
+	    // Query
+	    System.out.println("\n* Use the database command:");
+	    String query = "for $x in doc('" + DB_PATH + "')//nodo/adios return data($x)";
+	    System.out.println(new XQuery(query).execute(context));
+	   
+	    // Close the database context
+	    context.close();
+	  }
 }
